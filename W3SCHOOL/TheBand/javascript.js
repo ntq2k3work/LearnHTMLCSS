3c59c746da5453a407c1d2e4606cc5ccec53bfcd
ntq2k3work <ntq2k3.work@gmail.com>
2023-03-09 17:40:47 +0700

    Hoàn thành

--- W3SCHOOL/TheBand/javascript.js
+++ W3SCHOOL/TheBand/javascript.js
@@ -1,10 +1,31 @@
-var list_img = document.querySelectorAll('.slider_section');
-function auto (){
-    list_img.forEach(element => {
-        if(element.style.display == 'block') element.style.display = 'none';
-        else element.style.display = 'block'
-    });
+var click_buy = document.querySelectorAll('button.ticket_buy');
+var on_modal = document.querySelector('.modal');
+var close_modal = document.querySelector('.modal_close');
+var modal_container = document.querySelector('.modal_container');
+var modal = document.querySelector('.modal');
+click_buy.forEach(function(values){
+    values.onclick = function(){
+        on_modal.classList.add('open');
+    }
+})
+function rm_modal (){
+    on_modal.classList.remove('open');
 }
-// setInterval(function() {
-//     auto()
-// },1000)
+close_modal.addEventListener('click',rm_modal);
+modal.addEventListener('click',rm_modal);
+
+modal_container.onclick = function(e){
+    e.stopPropagation();
+}
+
+var menu_phone = document.querySelector('.menu_icon_phone');
+var header =document.querySelector('header');
+var li_last = document.querySelector('nav .menu > li:last-child > a')
+console.log(li_last);
+menu_phone.onclick = function(){
+    let height = header.clientHeight;
+    if(height === 46) header.classList.add('header_phone');
+    else header.classList.remove('header_phone');
+    li_last.innerHTML = "Merch"
+    li_last.setAttribute('href','')
+}
